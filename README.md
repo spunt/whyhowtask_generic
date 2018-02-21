14e4d20c37c5549285064b06fcae83fb14e1faa4
Bob Spunt <bobspunt@gmail.com>
2018-02-20 21:05:10 -0800

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # whyhowtask_generic
-Generic MATLAB/Psychtoolbox code for running flavors of the Why/How Task
+
+Generic MATLAB/Psychtoolbox code for running custom editions of the Why/How Task, aka, the Levels of Inference (LOI) Task.
+
+## Basic Usage
+
+### Step 1: Build a design file
+
+This should be an Excel (xlsx, ideally) spreadsheet like the `design_test1.xlsx` file included in this repository. Trials are represented by different rows in the spreadsheet.
+
+### Step 2: Customize defaults in `task_defaults.m`
+
+The `import_design.m` tool assumes the `task_defaults.m` file is in the same folder as it. The most important defaults to set before importing are:
+
+- path definitions under `defaults.path`. For these, make sure your raw image files are located under the path defined in `defaults.path.rawimages`.
+- timing information (all in seconds), as these are used to determine if there if the onsets between successive blocks/trials are long enough:
+
+    - `firstISI`
+    - `maxDur`
+    - `inblockreminderDur`
+    - `preblockquestionDur`
+
+- the `defaults.screenres` parameter controls the resolution of the presentation slides that are saved in a mat-file (i.e., preloaded) when importing your design file
+
+The other parameters are important to be aware of when you are preparing to run the actual experiment, but can (probably) be ignored for the design import step.
+
+### Step 3: Basic usage of `import_design.m`
+
+To import the design in the file `design_test1.xlsx` and save the stimulus slides:
+
+```matlab
+import_design('design_test1.xlsx')
+```
+
+You can also do a "dry run" of the import process where nothing gets saved at the end, which can be useful for validating your design file:
+
+```matlab
+import_design('design_test1.xlsx', 1)
+```
+
+### Step 4: Run the task with your imported design
+
+If all went well, you should now be able to run your custom design with the `run_task` function.
--- task_defaults.m
+++ task_defaults.m
@@ -7,6 +7,33 @@ defaults.screenres      = [1200 900];   % primarily used to set the resolution
                         % of the slides constructed when initially loading
                         % the trial_definition spreadsheet
 
+% Timing (all in seconds)
+%==========================================================================
+defaults.preblockquestionDur = 2.25;   % dur of preblock question presentation
+defaults.maxDur         = 2;      % (max) dur of every response trial
+defaults.inblockreminderDur  = .50;   % dur of reminder cue presented prior to every trial
+                                      % except the first within each block
+defaults.firstISI       = 0.15;   % dur of interval between preblock question and
+                                  % first trial of each block
+defaults.endduration    = 8;      % dur of fixation period after last trial of last block
+defaults.ignoreDur      = 0.15;   % dur after trial presentation in which
+                                  % button presses are ignored (this is
+                                  % useful when participant provides a late
+                                  % response to the previous trial)
+                                  % DEFAULT VALUE = 0.15
+defaults.TR             = 1;      % Your TR (in secs)
+
+
+% Paths
+%==========================================================================
+defaults.path.base      = fileparts(mfilename('fullpath'));
+defaults.path.data      = fullfile(defaults.path.base, 'data');
+defaults.path.rawimages = fullfile(defaults.path.base, 'raw_stimuli');
+% defaults.path.rawimages = '/Users/bobspunt/Github/research-projects/conte-renewal-lois/stimuli';
+defaults.path.stim      = fullfile(defaults.path.base, 'slides');
+defaults.path.design    = fullfile(defaults.path.base, 'design');
+
+
 % Response Keys
 %==========================================================================
 defaults.escape         = 'ESCAPE'; % escape key (to exit early)
@@ -20,32 +47,6 @@ defaults.valid_keys     = {'1!' '2@' '3#' '4$'}; % valid response keys
 % two keys. If the subject is using a button box, it may be desirable to
 % include all buttons on the box in case the subject winds up having their
 % fingers on the wrong keys.
-
-% Timing (all in seconds)
-%==========================================================================
-defaults.TR             = 1;      % Your TR (in secs)
-defaults.preblockquestionDur = 2.25;   % dur of preblock question presentation
-defaults.maxDur         = 2;      % (max) dur of every response trial
-defaults.inblockreminderDur  = .50;   % dur of reminder cue presented prior to every trial
-                                      % except the first within each block
-defaults.endduration    = 8;      % dur of fixation period after last trial of last block
-defaults.firstISI       = 0.15;   % dur of interval between preblock question and
-                                  % first trial of each block
-defaults.ignoreDur      = 0.15;   % dur after trial presentation in which
-                                  % button presses are ignored (this is
-                                  % useful when participant provides a late
-                                  % response to the previous trial)
-                                  % DEFAULT VALUE = 0.15
-
-
-% Paths
-%==========================================================================
-defaults.path.base      = fileparts(mfilename('fullpath'));
-defaults.path.data      = fullfile(defaults.path.base, 'data');
-defaults.path.rawimages = fullfile(defaults.path.base, 'raw_stimuli');
-% defaults.path.rawimages = '/Users/bobspunt/Github/research-projects/conte-renewal-lois/stimuli';
-defaults.path.stim      = fullfile(defaults.path.base, 'slides');
-defaults.path.design    = fullfile(defaults.path.base, 'design');
 
 % Text Display Parameters
 %==========================================================================
